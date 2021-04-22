1e296adfc685b076e08b22bb3e4f5dcc748bbf6d
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2021-04-22 14:49:36 +0100

    [root] functions for plots included.

--- analyseRootOutput.C
+++ analyseRootOutput.C
@@ -45,3 +45,83 @@ void summary(TString fname) {
     std::cout << "pos z : " << *pz << std::endl;
   }
 }
+
+void projectXY(std::string fnamelist) {
+  std::vector<std::string> rootfiles;
+  std::string fname;
+  ifstream infile; // read list of root files from file
+  infile.open(fnamelist.data());
+  if (!infile) {
+    cout << "Error file list " << fnamelist.data() << endl;
+    exit(1);
+  }
+  while (infile >> fname)
+    rootfiles.push_back(fname);
+  infile.close();
+
+  TChain* chScore = new TChain("Score");
+  for (auto fn : rootfiles) {
+    chScore->Add(fn.data());
+  }
+
+  TTreeReader nt1(chScore);
+
+  TTreeReaderValue<double> px(nt1, "Posx");
+  TTreeReaderValue<double> py(nt1, "Posy");
+    
+  // event loop
+  TCanvas c1("c1", "Picture");
+  TH2D* hist = new TH2D("profile", "2D Profile", 100, -0.02, 0.02, 100, -0.02, 0.02);
+  while (nt1.Next())
+  {
+    hist->Fill(*px, *py);
+  }
+  hist->GetXaxis()->SetTitle("x-position [mm]");
+  hist->GetYaxis()->SetTitle("y-position [mm]");
+  hist->GetZaxis()->SetTitle("Entries");
+  hist->GetXaxis()->SetTitleOffset(1.5);
+  hist->GetYaxis()->SetTitleOffset(1.8);
+  hist->GetZaxis()->SetTitleOffset(1.4);
+  hist->Draw("surf3");
+  c1.SaveAs("pic.png");
+
+}
+
+void projectZ(std::string fnamelist)
+{
+  std::vector<std::string> rootfiles;
+  std::string fname;
+  ifstream infile; // read list of root files from file
+  infile.open(fnamelist.data());
+  if (!infile) {
+    cout << "Error file list " << fnamelist.data() << endl;
+    exit(1);
+  }
+  while (infile >> fname)
+    rootfiles.push_back(fname);
+  infile.close();
+
+  TChain* chScore = new TChain("Score");
+  for (auto fn : rootfiles) {
+    chScore->Add(fn.data());
+  }
+    
+  TTreeReader nt1(chScore);
+     
+  TTreeReaderValue<double> pz(nt1, "Posz");
+
+  // event loop 
+  TCanvas c1("c1", "Picture");
+  TH1D* hist = new TH1D("profilez", "Z Profile", 100, -0.29, 0.29);
+  while (nt1.Next())
+  {
+    hist->Fill(*pz);
+  }
+  hist->GetXaxis()->SetTitle("z-position [mm]");
+  hist->GetYaxis()->SetTitle("Entries");
+  hist->SetStats(0);
+  hist->Draw();  
+  c1.SaveAs("picZ.png");
+    
+}
+
